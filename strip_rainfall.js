1c0d8136363f5ae0ce7317c1cd7d6a93dfe5e7b9
Thijmen de Valk <thijmen@wingpictures.nl>
2018-03-08 15:09:45 +0100

    Rainfall effect.

--- strip_rainfall.js
+++ strip_rainfall.js
@@ -6,17 +6,6 @@ var client = new OPC('localhost', 7890);
 var glowPixels = new Array();
 var randomPixel;
 
-/* function getGlow(pixel) {
-  for(var i = 0; i < glowPixels.length; i++) {
-    if(glowPixels[i].pixel == pixel) {
-      console.warn(pixel);
-      return 0;
-    }
-    else {
-      return 1;
-    }
-  }
-}
 function getRandomPixel() {
   return Math.floor(Math.random() * 960);
 }
@@ -24,9 +13,10 @@ function pickPixel() {
   var millis;
   var randomPixelInfo;
   var randomPixel;
+  var duration;
   randomPixel = getRandomPixel();
   millis = new Date().getTime() / 1000;
-  
+  duration = Math.floor(Math.random() * (1000 - 30 + 1)) + 30;
   randomPixelInfo = { pixel: randomPixel, time: millis };
   glowPixels.push(randomPixelInfo);
 
@@ -37,22 +27,22 @@ function pickPixel() {
           break;
       }
     }
-  },3000);
-} */
+  },duration);
+}
 function draw() {
     let millis = new Date().getTime();
-
+    var currentGlow = glowPixels.map(a => a.pixel);
+  
     for (var pixel = 0; pixel < 960; pixel++)
     {
         let saturation = 1;
         strip = Math.floor(pixel / 60);
         millisNew = (millis / 500) + strip;
-        hue = (0.05 * (Math.sin(millisNew) * -1) + 0.72 % 100);
-        random = Math.random();
-        if ( random > 0.99 ) {
+        hue = (0.05 * (Math.sin(millisNew) * -1) + millis / 50000 % 100);
+        if ( currentGlow.indexOf(pixel) > -1 ) {
           saturation = 0;
         }
-        colors = OPC.hsv(hue, saturation, 1);
+        colors = OPC.hsv(hue, 1, saturation);
         client.setPixel(pixel, colors[0], colors[1], colors[2]);
     }
     client.writePixels();
@@ -62,5 +52,5 @@ function logPixel() {
 }
 
 setInterval(draw, 175);
-// setInterval(pickPixel, 250);
+setInterval(pickPixel, 3);
 // setInterval(logPixel, 3000);
